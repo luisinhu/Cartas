e83a87c5220d1356d9115e602ac5ae2cd587cc41
DanzinNN <danielguimaraessouza8@gmail.com>
2023-03-25 16:28:22 -0300

    att

--- benzema.py
+++ benzema.py
@@ -1,6 +1,7 @@
 from classJogadores import Jogador
-
 benzema = Jogador("M", "benzema", "Real", "França")
 benzema.card()
 formiga = Jogador("F","formiga","Psg","Brasil")
 formiga.card()
+mbappe = Jogador ("M","Mbappé","PSG","França")
+mbappe.card()
