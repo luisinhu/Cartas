417b83728651b202120dfcbca9f8ccf74b6376f3
DanzinNN <danielguimaraessouza8@gmail.com>
2023-04-02 19:55:55 -0300

    Danzin é muito pik4 tá pae

--- main.py
+++ main.py
@@ -15,12 +15,60 @@ Debinha = Jogado("F","Debinha","Kansas City Current","Brasil",93,88,85,87)
 
 #sorteio das cartas
 lista_de_oponentes = ["benzema", "neymar", "pelé", "bappe","formiga","Marta","Debinha"]
-sorteio = random.choice(lista_de_oponentes)
-print("Esse será seu Oponente\n",sorteio)
+sorteio  =  random.randint ( 0 , len ( lista_de_oponentes ))
+sorteio  -=  1
+print ( lista_de_oponentes [ sorteio ])
+#chamando a carta
+if sorteio == 0:
+    print("Esse será seu oponente 1:\n")
+    benzema.imp()
+if sorteio == 1:
+    print("Esse será seu oponente 1:\n")
+    neymar.imp()
+if sorteio == 2:
+    print("Esse será seu oponente 1:\n")
+    pelé.imp()
+if sorteio == 3:
+    print("Esse será seu oponente 1:\n")
+    bappe.imp()
+if sorteio == 4:
+    print("Esse será seu oponente 1:\n")
+    formiga.imp()
+if sorteio == 5:
+    print("Esse será seu oponente 1:\n")
+    Marta.imp()
+if sorteio == 6:
+    print("Esse será seu oponente 1:\n")
+    Debinha.imp()
 
-sorteio2 = random.choice(lista_de_oponentes)
-print("Esse será seu Oponente\n",sorteio2)
 
+
+lista_de_oponentes2 = ["benzema", "neymar", "pelé", "bappe","formiga","Marta","Debinha"]
+sorteio2 =  random.randint ( 0 , len ( lista_de_oponentes2 ))
+sorteio2  -=  1
+print ( lista_de_oponentes2 [ sorteio2 ])
+#chamando a carta
+if sorteio2 == 0:
+    print("Esse será seu oponente 2:\n")
+    benzema.imp()
+if sorteio2 == 1:
+    print("Esse será seu oponente 2:\n")
+    neymar.imp()
+if sorteio2 == 2:
+    print("Esse será seu oponente 2:\n")
+    pelé.imp()
+if sorteio2 == 3:
+    print("Esse será seu oponente 2:\n")
+    bappe.imp()
+if sorteio2 == 4:
+    print("Esse será seu oponente 2:\n")
+    formiga.imp()
+if sorteio2 == 5:
+    print("Esse será seu oponente 2:\n")
+    Marta.imp()
+if sorteio2 == 6:
+    print("Esse será seu oponente 2:\n")
+    Debinha.imp()        
 
 
 
