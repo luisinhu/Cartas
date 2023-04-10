4d964332980a9ca81301fe8f6bcdca06a5109b0f
DanzinNN <danielguimaraessouza8@gmail.com>
2023-04-10 06:28:59 -0300

    atualização

---
+++ classejogador.py
@@ -0,0 +1,165 @@
+#informática
+#2 ano vespertino
+#Programação Orientada a Objetos
+#Alunos: Daniel Guimarães Souza 
+#Danielly Vitória Ferreira da Costa 
+#Luis Henrique Silva Botelho 
+#Marcos Aurélio
+class Jogado:
+#class 
+  #construtor
+    def __init__(self,nome,sexo,time,passe,fina, ritmo, drible, defesa, fisico):
+        #atributos
+        self.nome = nome
+        self.sexo = sexo
+        self.time = time
+        self.passe = passe
+        self.fina = fina
+        self.ritmo = ritmo
+        self.drible = drible
+        self.defesa=defesa
+        self.fisico=fisico
+    #função de mostrar a carta 
+    def imprimir (self):
+        print("="*15)
+        print(f"Nome:{self.nome}")
+        print(f"Sexo:{self.sexo}")
+        print(f"Time:{self.time}")
+        print(f"Passe:{self.passe}")
+        print(f"Finalização:{self.fina}")
+        print(f"Rtimo:{self.ritmo}")
+        print(f"Drible:{self.drible}")
+        print(F"Defesa:{self.defesa}")
+        print(f"Físico:{self.fisico}")
+        print("="*15)        
+    #função de comparar qual atributo é maior
+    def compara (self, oponente):
+        escolha_atribruto = input("Escolha um atributo para comparar\n[1]Drible\n[2]Passe\n[3]Ritmo\n[4]Finalização\n[5]Físico\n[6]Defesa\nR: ")
+        if escolha_atribruto == "1":
+          if self.drible > oponente.drible:
+              print(f"{self.nome} tem o drible {self.drible} maior que o drible de {oponente.nome} que é {oponente.drible}")
+              return oponente
+          elif self.drible == oponente.drible:
+            print(f"{self.nome} empatou com o(a) oponente {oponente.nome} no atributo {self.drible}")
+            return oponente
+
+          else:
+            print(f"{oponente.nome} tem o drible {oponente.drible} maior que o drible de {self.nome} que tem o valor de {self.drible}")
+            return oponente
+
+        if escolha_atribruto == "2":
+          if self.passe > oponente.passe:
+              print(f"{self.nome} tem o passe {self.passe} maior que o passe de {oponente.nome} que é {oponente.passe} ")
+              return oponente
+          elif self.passe == oponente.passe:
+            print(f"{self.nome} empatou com o(a) oponente {oponente.nome} no atributo {self.passe}")
+            return oponente
+            
+          else:
+            print(f"{oponente.nome} tem o passe {oponente.passe} maior que o passe de {self.nome} que é {self.passe}  ")
+            return oponente
+                
+        if escolha_atribruto == "3":
+          if self.ritmo > oponente.ritmo:
+            print(f"{self.nome} tem o ritmo {self.ritmo} maior que o ritmo de{oponente.nome} que é {oponente.ritmo} ")
+            return oponente
+          elif self.passe == oponente.passe:
+            print(f"{self.nome} empatou com o(a) oponente {oponente.nome} no atributo {self.passe}")
+            return oponente
+            
+            
+          else:
+              print(f"{oponente.nome} tem o ritmo {oponente.ritmo} mairo que o ritmo de {self.nome} que é {self.ritmo}")
+              return oponente
+        if escolha_atribruto == "4":
+          if self.fina > oponente.fina:
+            print(f"{self.nome} tem a finalização {self.fina} maior que a finalização de {oponente.nome} que é {oponente.fina} ")
+            return oponente
+          elif self.fina == oponente.fina:
+            print(f"{self.nome} empatou com o(a) oponente {oponente.nome} no atributo {self.fina}")
+            return oponente
+          else:
+            print(f"{oponente.nome} tem a finalização {oponente.fina} maior que a finalização de {self.nome} que é {self.fina}")
+            return oponente
+        
+        if escolha_atribruto == "5":
+            if  self.fisico > oponente.fisico:
+              print(f"{self.nome} tem o físico {self.fisico} maior que o físico de {oponente.nome} que é {oponente.fisico}")
+              return oponente
+            elif self.fisico == oponente.fisico:
+              print(f"{self.nome} empatou com o(a) oponente {oponente.nome} no atributo {self.fisico}")
+              return oponente
+    
+            
+            else:
+                print(f"{oponente.nome} tem o físico {oponente.fisico} maior que o físico de {self.nome} que é {self.fisico}")
+                return oponente
+        
+        if escolha_atribruto == "6":
+            if self.defesa > oponente.defesa:
+              print(f"{self.nome} tem a defesa {self.defesa} maior que a defesa de {oponente.nome} que é {oponente.defesa}")
+              return oponente
+            elif self.defesa == oponente.defesa:
+              print(f"{self.nome} empatou com o(a) oponente {oponente.nome} no atributo {self.defesa} ")
+           
+            else:
+              print(f"{oponente.nome} tem a defesa {oponente.defesa} maior que a defesa de {self.nome} que é {self.defesa}")
+              return oponente
+     #função de fazer a competição e mostrar o ganhador -Dani
+    def definindo_ganhador(self, oponente):
+      escolher = int(input("Escolha um atributo para competir\n[1]Drible\n[2]Passe\n[3]Ritmo\n[4]Finalização\n[5]Físico\n[6]Defesa\nR: "))
+      if escolher == 1:
+        if self.drible > oponente.drible:
+          print(f"{self.nome} ganhou de {oponente.nome} no atributo drible")
+          
+        elif self.drible == oponente.drible:
+          print(f"{self.nome} empatou com o oponente {oponente.nome}")
+        
+        else: 
+          print(f"{oponente.nome} ganhou de {self.nome} no atributo drible")
+      
+      if escolher == 2:
+        if self.passe > oponente.passe:
+          print(f"{self.nome} ganhou de {oponente.nome} no atributo passe")
+        
+        elif self.passe == oponente.passe:
+          print(f"{self.nome} empatou com o oponente {oponente.nome}")
+          
+        else:
+          print(f"{oponente.nome} ganhou de {self.nome} no atributo passe")
+      if escolher == 3:
+        if self.ritmo > oponente.ritmo:
+           print(f"{self.nome} ganhou de {oponente.nome} no atributo ritmo")
+
+        elif self.ritmo == oponente.ritmo:
+          print(f"{self.nome} empatou com o oponente {oponente.nome}")
+          
+        else:
+          print(f"{oponente.nome} ganhou de {self.nome} no aributo ritmo")
+      if escolher == 4:
+        if self.fina > oponente.fina:
+          print(f"{self.nome} ganhou de {oponente.nome} no atributo finalização")
+
+        elif self.fina == oponente.fina:
+          print(f"{self.nome} empatou com o oponente {oponente.nome}")
+          
+        else:
+           print(f"{oponente.nome} ganhou de {self.nome} no atributo finalização")
+      if escolher == 5:
+        if self.fisico > oponente.fisico:
+           print(f"{self.nome} ganhou de {oponente.nome} no atributo físico")
+
+        elif self.fisico == oponente.fisico:
+          print(f"{self.nome} empatou com o oponente {oponente.nome}")
+          
+        else:
+           print(f"{oponente.nome} ganhou de {self.nome} no atributo físico")
+      if escolher == 6:
+        if self.defesa > oponente.defesa:
+           print(f"{self.nome} ganhou de {oponente.nome} no atributo defesa")
+          
+        elif self.defesa == oponente.defesa:
+          print(f"{self.nome} empatou com o oponente {oponente.nome}")
+          
+        else:
+           print(f"{oponente.nome} ganhou de {self.nome} no atributo defesa")
--- main.py
+++ main.py
@@ -1,89 +1,153 @@
-from jogador import Jogado
+from classejogador import Jogado
 import random
 import time
+import os
+#informática
+#2 ano vespertino
+#Programação Orientada a Objetos
+#Alunos 
+#Daniel Guimarães Souza 
+#Danielly Vitória Ferreira da Costa 
+#Luis Henrique Silva Botelho  
+#Marcos Aurélio
 
-#criando os objetos
-benzema = Jogado("M", "Benzema", "Real", "França",90,97,92,94,45,90)
-neymar = Jogado("M", "Neymar", "PSG", "Brasil",87,87,90,95,39,64)
-pelé = Jogado("M", "Pelé", "Santos", "Brasil",96,96,99,76,60,76)
-bappe = Jogado("M", "Mbappe", "PSG", "França",88,96,99,98,44,87)
-formiga = Jogado("F","Formiga","PSG","Brasil",80,78,76,77,90,92)
-Marta = Jogado("F","Marta","Orlando Pride","Brasil",90,89,85,96,34,70)
-Debinha = Jogado("F","Debinha","Kansas City Current","Brasil",93,88,85,87,50,77)
+#inicio -Daniel
+print("Olá Você está jogando um jogo de super trunfo sobre o tema de futebol\nEspero que se divirta\n")
 
+primeira_acao = int(input("Escolha uma opção.\n[1]Iniciar\n[2]Programadores\nR: "))
+while primeira_acao != 1 and primeira_acao != 2 :
+    print("Tente novamente")
+    primeira_acao = int(input("Escolha uma opção.\n[1]Iniciar\n[2]Programadores\nR: "))
+if primeira_acao == 1:
+    loading = ("Carregando: {----------}")
+    for i in range(11):
+        os.system('clear')
+        print (loading)
+        print ("")
+        loading = loading.replace("-","=",1)
+        time.sleep(0.2)
 
-
-lista_de_oponentes = [benzema, neymar, pelé ,bappe,formiga,Marta,Debinha]
-sorteio  =  random.randint ( 0 , len( lista_de_oponentes ))
-sorteio -= 1
-#chamando as cartas
-if sorteio == 0:
-   print("Essa é sua carta 1:")
-   benzema.imp()
-if sorteio == 1:
-    print("Essa é sua carta 1:")
-    neymar.imp()
-if sorteio == 2:
-    print("Essa é sua carta 1:")
-    pelé.imp()
-if sorteio == 3:
-    print("Essa é sua carta 1:")
-    bappe.imp()
-if sorteio == 4:
-    print("Essa é sua carta 1:")
-    formiga.imp()
-if sorteio == 5:
-    print("Essa é sua carta 1:")
-    Marta.imp()
-if sorteio == 6:
-    print("Essa é sua carta 1:")
-    Debinha.imp()
+if primeira_acao == 2:
+    print('\033[1m'"="*15)
+    print("Nossos programadores são:\nDaniel\nDanielly\nLuis\nMarcos")
+    print("="*15,'\033[0;0m')
+    time.sleep(4)
+    os.system('clear')
+    print("Vamos lá")
 
 
-#sorteio da segunda carta
-lista_de_oponentes2 = [benzema, neymar, pelé ,bappe,formiga,Marta,Debinha]
-sorteio2  =  random.randint ( 0 , len( lista_de_oponentes2 ))
-sorteio2 -= 1
+#criando os objetos 
+Jogador1 = Jogado("Benzema","Masculino","Real Madrid",90,97,92,94,45,90)
+Jogador2 = Jogado("Neymar","Masculino","PSG",87,87,90,95,39,64)
+jogador3 = Jogado("Pelé","Masculino","Santos",93,96,96,96,60,76)
+jogador4 = Jogado("Mbappé","Masculino","PSG",88,96,99,98,44,87)
+jogador5 = Jogado("Formiga","Feminino","PSG",80,78,76,77,90,92)
+jogador6 = Jogado("Marta","Feminino","Orlando Pride",90,89,85,96,34,70)
+jogador7 = Jogado("Debinha","Feminino","Kansas City Current",93,88,85,87,50,77)
 
-#chamando as cartas
-if sorteio2 == 0:
-    print("Essa é sua carta 2:")
-    benzema.imp()
-    benzema.compara(sorteio )
-if sorteio2 == 1:
-    print("Essa é sua carta 2:")
-    neymar.imp()
-    neymar.compara(sorteio)
-if sorteio2 == 2:
-    print("Essa é sua carta 2:")
-    pelé.imp()
-    pelé.compara(sorteio)
-if sorteio2 == 3:
-    print("Essa é sua carta 2:")
-    bappe.imp()
-    bappe.compara(sorteio)
-if sorteio2 == 4:
-    print("Essa é sua carta 2:")
-    formiga.imp()
-    formiga.compara(sorteio)
-if sorteio2 == 5:
-    print("Essa é sua carta 2:")
-    Marta.imp()
-    Marta.compara(sorteio)
-if sorteio2 == 6:
-    print("Essa é sua carta 2:")
-    Debinha.imp()
-    Debinha.compara(sorteio)
+#lista cartas
+lista_cartas = [Jogador1, Jogador2, jogador3, jogador4, jogador5, jogador6, jogador7]
+#lista do usuário
+carta_user = []
+#lista da máquina
+carta_maquina = []
+#escolha da carta usuário -Daniel
+os.system('cls')
+escolher = int(input(f"Escolha de 1 a {len(lista_cartas)}\nR: "))
+escolher = random.randint(0, len(lista_cartas))
+escolher -= 1
+carta_user = lista_cartas[escolher]
+
+if escolher == 0:
+  lista_cartas.remove(Jogador1)
+  print("Essa é sua carta escolhida")
+  
+  Jogador1.imprimir()
+  
+
+if escolher == 1: #neymar
+  lista_cartas.remove(Jogador2)
+  print("Essa é sua carta escolhida")
+  Jogador2.imprimir()
+if escolher == 2: #pelé
+  lista_cartas.remove(jogador3)
+  print("Essa é sua carta escolhida")
+  jogador3.imprimir()
+
+if escolher == 3: #mbappe
+  lista_cartas.remove(jogador4)
+  print("Essa é sua carta escolhida")
+  jogador4.imprimir()
 
 
+if escolher == 4: #formiga
+  lista_cartas.remove(jogador5)
+  print("Essa é sua carta escolhida")
+  jogador5.imprimir()
+
+if escolher == 5: #marta
+  lista_cartas.remove(jogador6)
+  print("Essa é sua carta escolhida")
+  jogador6.imprimir()
+
+if escolher == 6: #debinha
+  lista_cartas.remove(jogador7)
+  print("Essa é sua carta escolhida")
+  jogador7.imprimir()
 
 
+#sorteio da máquina - Marcos
+
+sorteado  = random.randint(0, len(lista_cartas))
+sorteado -= 1
+carta_maquina = lista_cartas[sorteado]
+
+if sorteado == 0:
+  print("Carta da máquina")
+  Jogador1.imprimir()
+  
+if  sorteado == 1:
+  print("Carta da máquina")
+  Jogador2.imprimir()
+
+if sorteado == 2:
+  print("Carta da máquina")
+  jogador3.imprimir()
+  
+if sorteado == 3:
+  print("Carta da máquina")
+  jogador4.imprimir()
+  
+if sorteado == 4:
+  print("Carta da máquina")
+  jogador5.imprimir()
+
+if sorteado == 5:
+  print("Carta da máquina")
+  jogador6.imprimir()
+
+if sorteado == 6:
+  print("Carta da máquina")
+  jogador7.imprimir()
 
 
+#Comparação dos atributos Daniel e Marcos
+time.sleep(3)
+os.system('cls')
+carta_user.compara(oponente=carta_maquina)
 
-
-
-
-
+#competição Dani
+time.sleep(4)
+os.system('cls')
+carta_user.definindo_ganhador(oponente= carta_maquina)
+#finalzin legal
+time.sleep(3)
+loading = ("Desligando: {----------}")
+for i in range(11):
+  os.system('cls')
+  print (loading)
+  print ("")
+  loading = loading.replace("-","=",1)
+  time.sleep(0.2)
 
 
